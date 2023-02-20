5f091eae9dc18e52590e0ad38f2b24079cb78ab5
Aakanksha <123055596+Aakanksha-H@users.noreply.github.com>
2023-02-20 09:44:29 +0530

    Update CountSmallerNumber.java

--- CountSmallerNumber.java
+++ CountSmallerNumber.java
@@ -1,12 +1,21 @@
 public class CountSmallerNumber {
     public void countSmallerNumber() {
-        int arry [] = { 13 , 12, 13 , 12, 13 , 12, 15, 18,  15, 18, 15, 18, 15, 18};
+        int[] arry = {4,45,41,7,1,5,1,1};
+        int count = 0;
         int min = arry[0];
-        for (int i = 1; i < arry.length; i++) {
-            if (min > arry[i]) {
-                min = arry[i];
+            for (int i = 1; i < arry.length; i++) {
+                if (min > arry[i]) {
+                    min = arry[i];
+                }
+            }
+        System.out.println("Min number in arry is " + min);
+        for(int i = 0; i < arry.length; i++)
+        {
+            if(arry[i] == min)
+            {
+                count++;
             }
         }
-        System.out.println("Min number in arry is " + min);
+        System.out.println("The count is " + count);
     }
 }
